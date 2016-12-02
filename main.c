92116b85f752702af2e573b10dacbbfc68c13880
lode <lodeameije@gmail.com>
2016-12-01 16:33:57 -0800

    add stm32f4_discovery.h from repository --> has all the peripherals from the discovery board.

--- main.c
+++ main.c
@@ -18,6 +18,8 @@
 #include "usbd_desc.h"
 #include "usbd_cdc_vcp.h"
 #include "usb_dcd_int.h"
+#include "stm32f4_discovery.h"
+
 
 volatile uint32_t ticker, downTicker;
 
@@ -74,6 +76,8 @@ int main(void)
 	/* Initialize USB, IO, SysTick, and all those other things you do in the morning */
 	init();
 
+	STM_EVAL_LEDInit(LED5);
+	STM_EVAL_LEDOn(LED5);
 
 	while (1)
 	{
@@ -248,3 +252,44 @@ void OTG_FS_WKUP_IRQHandler(void)
   }
   EXTI_ClearITPendingBit(EXTI_Line18);
 }
+
+
+/*
+void adc_configure(void){
+     ADC_InitTypeDef ADC_init_structure; //Structure for adc confguration
+     GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
+
+     //Clock configuration
+     RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);//The ADC1 is connected the APB2 peripheral bus thus we will use its clock source
+     RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);//Clock for the ADC port!! Do not forget about this one ;)
+
+    //Analog pin configuration
+     GPIO_initStructre.GPIO_Pin = GPIO_Pin_0;//The channel 10 is connected to PC0
+     GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
+     GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
+     GPIO_Init(GPIOC,&GPIO_initStructre);//Affecting the port with the initialization structure configuration
+
+    //ADC structure configuration
+     ADC_DeInit();
+     ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right;//data converted will be shifted to right
+     ADC_init_structure.ADC_Resolution = ADC_Resolution_12b;//Input voltage is converted into a 12bit number giving a maximum value of 4096
+     ADC_init_structure.ADC_ContinuousConvMode = ENABLE; //the conversion is continuous, the input data is converted more than once
+     ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;// conversion is synchronous with TIM1 and CC1 (actually I'm not sure about this one :/)
+     ADC_init_structure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;//no trigger for conversion
+     ADC_init_structure.ADC_NbrOfConversion = 1;//I think this one is clear :p
+     ADC_init_structure.ADC_ScanConvMode = DISABLE;//The scan is configured in one channel
+     ADC_Init(ADC1,&ADC_init_structure);//Initialize ADC with the previous configuration
+
+     //Enable ADC conversion
+     ADC_Cmd(ADC1,ENABLE);
+
+     //Select the channel to be read from
+     ADC_RegularChannelConfig(ADC1,ADC_Channel_10,1,ADC_SampleTime_144Cycles);
+}
+
+int adc_convert(void){
+ ADC_SoftwareStartConv(ADC1);//Start the conversion
+ while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));//Processing the conversion
+ return ADC_GetConversionValue(ADC1); //Return the converted data
+}
+*/
